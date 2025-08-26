4655af6658f80266689287dd0a9b37be46fcf380
abc <abc@example.com>
2025-08-26 18:26:32 +0300

    First Commit

---
+++ Entities/User.cs
@@ -0,0 +1,12 @@
+namespace Tripzy.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public byte Gender { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; } 
+}
